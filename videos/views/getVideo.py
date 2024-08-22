c2df04a4627aa72d4e2f7522cb412b2bb8f44440
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-08-22 17:55:16 +0530

    added api for handling movies

--- videos/views/getVideo.py
+++ videos/views/getVideo.py
@@ -7,27 +7,22 @@ from ..models import adsVideos
 @csrf_exempt
 def getVideos(request):
     if request.method == "GET":
-        print("Session ID:", request.COOKIES.get('sessionid'))
+        print("Session ID:", request.COOKIES.get("sessionid"))
         print(request.headers)
-        
+
         # return JsonResponse({"msg":"testing"})
         excludedIds = request.session.get("userSession", [])
-        print(excludedIds,"ids")
+        print(excludedIds, "ids")
         videoFileResponse = (
             videoFiles.objects.exclude(id__in=excludedIds).order_by("?").values()[:5]
         )
-        
+
         video_data = list(videoFileResponse)
-        if(len(video_data)>0):
+        if len(video_data) > 0:
             adsFileResponse = adsVideos.objects.order_by("?").values()[:1]
             ads_data = list(adsFileResponse)
             video_data.extend(ads_data)
-        # print(videoFileResponse)
-      
-        # print(len(video_data) == 0)
-        
-        # video_data.extend(ads_data)
-        # print(video_data)
+
         video_ids = (
             []
         )  # we wiil store all fetched ids here so that user could  not see the same content multiple times
@@ -45,14 +40,5 @@ def getVideos(request):
         print(request.session)
         return JsonResponse({"data": video_data}, status=200)
 
-        # responseVideofiles = videoFiles.objects.all().values(
-        #     "id", "path", "userId", "cretaedAt"
-        # )
-
-        # # # Convert QuerySet to a list of dictionaries
-        # data = list(responseVideofiles)
-
-        # # Return JSON response with the data
-        # return JsonResponse({"data": data}, status=200)
     else:
         return JsonResponse({"msg": "method not allowed"}, status=405)
