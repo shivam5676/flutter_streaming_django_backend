36d5b212990b8010abab2441ade606f9cd50546b
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-09-09 17:53:56 +0530

    starting mongo db database

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\mobile app developent\flutter streaming app\streaming_app_backend\venv
+set VIRTUAL_ENV=C:\Company Projects\mobile app developent\flutter streaming app\streaming_app_backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
