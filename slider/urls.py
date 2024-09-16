5e20c910625fdffc4aeb1baf9136f9c1968fc3c1
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-09-16 16:49:26 +0530

    added controllers for handling related tours

--- slider/urls.py
+++ slider/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views.getSlider import getSliders
 from .views.getMovieData import getMovieData
 from .views.getLayouts import getLayouts
+from .views.getDataRelatedToLayouts import getDataRelatedToLayOuts
 from django.contrib import admin
 
-urlpatterns = [path("getSliders/", getSliders), path("getMovieData/", getMovieData),path("getLayouts/",getLayouts)]
+urlpatterns = [path("getSliders/", getSliders), path("getMovieData/", getMovieData),path("getLayouts/",getLayouts),path("getLayoutData/<movieID>",getDataRelatedToLayOuts)]
--- slider/views/getDataRelatedToLayouts.py
+++ slider/views/getDataRelatedToLayouts.py
@@ -1,8 +1,26 @@
-from streaming_app_backend.mongo_client import layouts_collection
+from streaming_app_backend.mongo_client import layouts_collection, movies_collection
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from bson import ObjectId
 
 
-def getDataRelatedToLayOuts():
+@csrf_exempt
+def getDataRelatedToLayOuts(request,movieID):
+    print(movieID)
     # we will get layout id and then we will fetch all the related movies and thier shorts there
     # we also can set limit for that we need to get starting and ending ids means we can decide how much data we have to send per request(optional)
+    if request.method == "GET":
+        result = layouts_collection.find({"_id": ObjectId(movieID)})
+        movieObj = []
+        for layout in result:
+            linkedMovies = layout["linkedMovies"]
+            for currentMovieId in linkedMovies:
+                #   print(currentMovieId)
+                movieData = movies_collection.find_one(
+                    {"_id": currentMovieId}, {"fileLocation": 1, "name": 1}
+                )
+                movieData['_id']=str(movieData['_id'])
+                movieObj.append(movieData)
+               
+        return JsonResponse({"moviesList": movieObj})
     return JsonResponse({"msg": "method not allowed"})
